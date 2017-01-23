764c52ade4ce1ea628d4876384c7d38b57a07ac2
Guangyu Wang <wanggy@umich.edu>
2017-01-22 22:52:58 -0500

    Update README.java

--- README.java
+++ README.java
@@ -1,5 +1,3 @@
-There are 2 things that I am aware of but did not solve in this project:
-
-1.I assume that the budget txt file ends with a new line at the end. The program directly writes to it without creating a new line.
-
-2.I used three signature strings "!!!", "###" and "@@@" for command types from the client to the server, I assumed that there will be no categories named by these. I know this is a bad habit but I didn't figure out how to get around it.
+This is a simple budget control tool implemented using JAVA. 
+  
+As a course project, this is not the version for submission, but it is still compilable and could fully function.
