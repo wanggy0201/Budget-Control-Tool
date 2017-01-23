a82012ff826c7d6086d2e22c9cdd21f34625d6a6
Guangyu Wang <wanggy@umich.edu>
2017-01-22 22:54:45 -0500

    Update README.java

--- README.java
+++ README.java
@@ -1,3 +1,5 @@
 This is a simple budget control tool implemented using JAVA. 
   
 As a course project, this is not the version for submission, but it is still compilable and could fully function.
+
+Source codes could be found in /src, and the files that are read and written to are in the root directory.
